374b17e67c54456f820b26f93129888c0b9495cb
kennetmakannasi <kennethdiscord010@gmail.com>
2024-11-27 20:49:13 +0700

    css fixes

--- components/custom/navbar/navbar.tsx
+++ components/custom/navbar/navbar.tsx
@@ -7,15 +7,15 @@ export function Navbar(){
         <div className="inline-flex py-3">
             <Input type="text" placeholder="search"/>
             <div className="float-right absolute right-20">
-                <div className="inline-flex">
-                    <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                    <div className="ml-5">
-                        <p className="text-gray-600 font-semibold">Lorem</p>
-                        <p className="text-xs text-gray-600">Admin</p>    
-                    </div>
+                <div className="inline-flex opacity-0 lg:opacity-100">
+                        <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>    
+                        <div className="ml-5">
+                            <p className="text-gray-600 font-semibold">Lorem</p>
+                            <p className="text-xs text-gray-600">Admin</p>    
+                        </div>
                 </div>
             </div>
         </div>    
