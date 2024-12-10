edaaf8e9b0c971f981103fac8c44276b4c52e614
bluebanana21 <m.affanfahrozi@gmail.com>
2024-12-10 18:36:04 +0700

    added majors data table and form

---
+++ app/majors/columns.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string;
+  kejuruan: string;
+  ketua: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "id",
+    header: "id",
+  },
+  {
+    accessorKey: "kejuruan",
+    header: "Kejuruan",
+  },
+  {
+    accessorKey: "ketua",
+    header: "Ketua jurusan",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const kejuruan = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(kejuruan.id)}
+            >
+              Copy kejuruan ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
---
+++ app/majors/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { AppSidebar } from "@/components/custom/sidebar/app-sidebar";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Payment, columns } from "./columns";
+import { DataTable } from "./data-table";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { Link, School, Users } from "lucide-react";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      kejuruan: "PPLG",
+      ketua: "Miranda SPD",
+    },
+    {
+      id: "728ed52f",
+      kejuruan: "PPLG",
+      ketua: "Miranda SPD",
+    },
+    {
+      id: "728ed52f",
+      kejuruan: "PPLG",
+      ketua: "Miranda SPD",
+    },
+  ];
+}
+
+export default async function Page() {
+  const data = await getData();
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <ScrollArea>
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 shadow-md dark:shadow-none">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Kejuruan</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          <div className="flex items-center justify-between p-4 pt-4">
+            <div className="rounded-xl bg-muted/90 border dark:border-none dark:bg-muted/50 col-span-1 aspect-[1/0.6] flex items-center justify-between p-4">
+              <div className="flex items-center justify-center p-2 rounded-lg bg-muted/70">
+                <School
+                  height={52}
+                  width={52}
+                  className="text-[hsl(var(--chart-2))]"
+                />
+              </div>
+
+              <div className="text-right px-2">
+                <h3 className="text-2xl font-bold text-[hsl(var(--chart-2))]">
+                  5
+                </h3>
+                <p className="text-sm text-muted-foreground">Jurusan</p>
+              </div>
+            </div>
+            <Button
+              variant="outline"
+              className="flex items-center justify-between p-4"
+            >
+              <a href="/majors/create">Add new major +</a>
+            </Button>
+          </div>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-4">
+            <div className="rounded-xl bg-muted/90 border dark:border-none dark:bg-muted/50 col-span-4">
+              <DataTable columns={columns} data={data} />
+            </div>
+          </div>
+        </ScrollArea>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
